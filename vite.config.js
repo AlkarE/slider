dd51d5f6c431c33cbc434464e81fc9663795cc9d
alkar <alkar.ew@gmail.com>
2022-12-28 12:19:57 +0100

    initial

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite"
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./scss/_vars.scss";`,
+      },
+    },
+  },
+})
